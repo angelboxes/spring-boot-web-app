b1aefcbfd45d08f97e7f176395092f37b1329e40
Angel Cajas <angel.cj.o@gmail.com>
2020-07-03 13:06:17 -0600

    Up until Step 07

--- src/main/java/com/angelboxes/springbootwebapp/controller/LoginController.java
+++ src/main/java/com/angelboxes/springbootwebapp/controller/LoginController.java
@@ -3,16 +3,22 @@ package com.angelboxes.springbootwebapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String loginMessage(@RequestParam String name, ModelMap model) {
+    public String loginMessage(ModelMap model) {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginMessage(ModelMap model,   @RequestParam String name) {
         System.out.println("The name is " + name);
         model.addAttribute("name", name);
-        return "login";
+        return "welcome";
     }
 
 }
--- src/main/webapp/views/jsp/login.jsp
+++ src/main/webapp/views/jsp/login.jsp
@@ -1,8 +1,12 @@
 <html>
 <head>
-<title>Web application</title>
+    <title>Web application</title>
 </head>
 <body>
-My first jsp!! Welcome ${name}!
+<form method="post">
+    Name: <input name="name" type="text">
+    Password:<input name="pasword" type="password">
+    <input type="submit">
+</form>
 </body>
 </html>
